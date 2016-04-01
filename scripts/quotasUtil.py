eaff000c1e530bcc78d540fa40361efb0ffaa7c6
Evan Sarmiento <esarmien@g.harvard.edu>
2016-04-01 10:02:12 -0400

    Add rshiny_ci3 to quotasUtil
    (cherry picked from commit 9ae5073f739017b6c6551d1f21bf64218a9ee87e)

--- scripts/quotasUtil.py
+++ scripts/quotasUtil.py
@@ -125,7 +125,7 @@ parser.add_argument('-a', '--action', required=True, choices=['A', 'D', 'M', 'S'
 parser.add_argument('-g', '--group', required=True,
                     help="Name of the group.")
 parser.add_argument('-v', '--volume', choices=['projects', 'projects_nobackup',
-                    'projects_ci3', 'projects_nobackup_ci3', 'www'],
+                    'projects_ci3', 'projects_nobackup_ci3', 'www', 'rshiny_ci3'],
                     help="The NetApp volume.")
 parser.add_argument('-s', '--size',
                     help="Size of the disk quota.")
