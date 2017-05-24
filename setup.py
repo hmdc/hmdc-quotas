eb388c868f16c6008c730a41f829cc19b02a5e1f
bfrank <bradley.frank@gmail.com>
2017-05-24 12:58:08 -0400

    upped version to 1.6
    (cherry picked from commit d049b97931dd2a28d3cec9ef4aedfe9ccdf9f80b)

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(author='Bradley Frank',
       requires=['ConfigParser','hmdclogger','humanize','re','sys'],
       scripts=['scripts/quotasUtil.py'],
       url='https://github.com/hmdc/hmdc-quotas',
-      version='1.5',
+      version='1.6',
 )
